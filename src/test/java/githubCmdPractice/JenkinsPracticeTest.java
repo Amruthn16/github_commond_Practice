7618fd08ac7401a983786a389422c096c7902530
Amruthn16 <amruthgowda36@gmail.com>
2023-03-10 17:54:22 +0530

    d1

--- src/test/java/githubCmdPractice/JenkinsPracticeTest.java
+++ src/test/java/githubCmdPractice/JenkinsPracticeTest.java
@@ -1,11 +1,19 @@
 package githubCmdPractice;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class JenkinsPracticeTest{
 
 	@Test
 	public void name() {
 		System.out.println("Jenkin_Test");
+		WebDriverManager.chromedriver().setup();
+		 WebDriver driver = new ChromeDriver();
+		 driver.get("https://www.google.com");
+		 
 	}
 }
